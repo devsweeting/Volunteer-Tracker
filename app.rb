0c2820316089f9372a100000d4a8ddd53108a94d
Devin Sweeting <devinsweeting@gmail.com>
2018-12-07 12:52:42 -0800

    create toure to add a new project to the homepage project list

--- app.rb
+++ app.rb
@@ -13,3 +13,21 @@ get ('/') do
   @volunteers = Volunteer.all
   erb(:index)
 end
+
+# --------  Project routes  ----------------
+post ('/create_project') do
+  title = params.fetch("title")
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:index)
+end
+
+get ('/create_project/:id') do
+  uid = params[:id].to_i
+  @movie = Movie.find(uid)
+  erb (:edit_project)
+end
+
+#  ----------------------------------------
