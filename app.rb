5acd1fc366dab7a11e34d5ec28a8cd64b0aa3b83
Devin Sweeting <devinsweeting@gmail.com>
2018-12-07 13:17:25 -0800

    Create feature to add volunteer and assign project_id

--- app.rb
+++ app.rb
@@ -52,3 +52,43 @@ delete ('/edit_project/:id') do
   erb (:index)
 end
 #  ----------------------------------------
+
+#  --------  Volunteer routes  ------------
+
+post ('/add_volunteer') do
+  name = params.fetch("name")
+  project_id = params.fetch("project_id")
+  volunteer = Volunteer.new({:name => name, :project_id => project_id, :id => nil})
+  volunteer.save
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:index)
+end
+
+# render a volunteer page
+get ('/edit_volunteer/:id') do
+  uid = params[:id].to_i
+  @project = Project.find(uid)
+  erb (:edit_project)
+end
+
+# update a project
+patch ('/edit_project/:id') do
+  title = params.fetch("title")
+  uid = params[:id].to_i
+  @project = Project.find(uid)
+  @project.update({:title => title, :id => nil})
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb (:index)
+end
+
+# delete a project
+delete ('/edit_project/:id') do
+  @project = Project.find(params.fetch("id").to_i())
+  @project.delete
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  binding.pry
+  erb (:index)
+end
